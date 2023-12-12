6e8eb7f405d7d8203d63c7adaea35412bce6a749
osman yaycioglu <osman.yaycioglu@gmail.com>
2023-12-12 09:35:26 +0300

    Initial commit

---
+++ node-configuration/src/test/java/com/training/micro/nodeconfiguration/NodeConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.micro.nodeconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
