01cbea0c39f2a1a9ad7b798da08a643d67046b24
osman yaycioglu <osman.yaycioglu@gmail.com>
2023-12-19 15:49:47 +0300

    day 5 3

--- ms-notify/src/main/java/training/microservices/msnotify/RefreshTestController.java
+++ ms-notify/src/main/java/training/microservices/msnotify/RefreshTestController.java
@@ -1,12 +1,14 @@
 package training.microservices.msnotify;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@RefreshScope
 public class RefreshTestController {
 
     @Value("${refresh.test}")
