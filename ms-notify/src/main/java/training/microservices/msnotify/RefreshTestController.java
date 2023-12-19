2c215b62225415decbbdb1ecb95af433a0cc59c7
osman yaycioglu <osman.yaycioglu@gmail.com>
2023-12-19 15:56:49 +0300

    day 5 3

--- ms-notify/src/main/java/training/microservices/msnotify/RefreshTestController.java
+++ ms-notify/src/main/java/training/microservices/msnotify/RefreshTestController.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 @RestController
 @RequestMapping("/test")
